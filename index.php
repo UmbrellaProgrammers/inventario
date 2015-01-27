1d8054d1dd5ebdee8dd7c071ce14db96e9b2d9c3
soporte-desarrollo1 <soporte.desarrollo1@ultragroupla.com>
2015-01-27 15:47:53 -0500

    borrar cambio 2

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 	<head>
 		<title>Inventario Nuevo</title>
-		<h2></h2>
 		<meta charset='utf-8'>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
